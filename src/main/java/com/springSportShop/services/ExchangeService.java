357bb15f6095d4f2ac56ce16611e1c1e171fb6c2
maciejszwaczka@gmail.com <maciejszwaczka@gmail.com>
2018-03-27 00:46:38 +0200

    added exchanging service

--- src/main/java/com/springSportShop/services/ExchangeService.java
+++ src/main/java/com/springSportShop/services/ExchangeService.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package com.springSportShop.services;
+import com.springSportShop.entities.*;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import org.springframework.web.bind.annotation.*;
@@ -12,8 +13,12 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.EntityManagerFactory;
 import java.util.List;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
 import java.util.ArrayList;
 import java.util.Date;
+import org.json.*;
+import java.util.stream.Stream;
 import javax.persistence.EntityTransaction;
 import org.springframework.transaction.annotation.Transactional;
 import javax.persistence.Persistence;
@@ -22,14 +27,42 @@ import org.springframework.beans.factory.annotation.Autowired;
  *
  * @author maciejszwaczka
  */
-/*@Service*/
+@Service
 public class ExchangeService {
     
-    /*Client client;
+    private Client client;
+    
+    private WebTarget target;
     
     public ExchangeService()
     {
         client=ClientBuilder.newClient();
-        client.target("");
-    }*/
+        target=client.target("http://api.nbp.pl/api/exchangerates/tables/A?format=json");
+    }
+    public void productPriceExchange(Product prod, String currency)
+    {
+        String requestStr=target.request(MediaType.TEXT_PLAIN).get(String.class);
+        int x = requestStr.indexOf("{");
+        requestStr = requestStr.substring(x);
+        JSONObject json = new JSONObject(requestStr.trim()); 
+        JSONObject request=new JSONObject(requestStr);
+        JSONArray rateArray=request.getJSONArray("rates");
+        for(int i=0;rateArray.length()>i;i++)
+        {
+            JSONObject obj=rateArray.getJSONObject(i);
+            if(obj.getString("code").equals(currency))
+            {
+                System.out.print(obj.getString("code"));
+                prod.setPrice(prod.getPrice()/obj.getDouble("mid"));
+                break;
+            }
+        }
+    }
+    public void listOfProductsPriceExchange(List<Product> prods, String currency)
+    {
+        for(Product prod:prods)
+        {
+            productPriceExchange(prod,currency);
+        }
+    }
 }
